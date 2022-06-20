9d8a37dcbd0ccde990a42797e5d72ea42fac8f81
Gutin7 <gustavo.7@rede.ulbra.br>
2022-06-20 15:28:10 -0300

    IAC JavaScript ConvertToDecimal e PROGRAMAÇÃO WEB Login e page admin

---
+++ Ulbra-main/2022.1/PROGRAMAÇÃO WEB/Aula_12/docker-code/www/codeigniter4/app/Views/client/detailsClient.php
@@ -0,0 +1,27 @@
+<h1>Detalhes do Clientes</h1>
+
+<table class="table">
+    <tr>
+        <th>Id do Client</th>
+        <td>
+            <?=$client['idClient']?>
+        </td>
+    </tr>
+    <tr>
+        <th>Nome</th>
+        <td>
+            <?=$client['name']?>
+        </td>
+    </tr>
+    <tr>
+        <th>Email</th>
+        <td>
+            <?=$client['email']?>
+        </td>
+    </tr>
+    <tr>
+        <th>Telefone</th>
+        <td>
+            <?=$client['phone']?>
+        </td>
+    </tr>
---
+++ Ulbra-main/2022.1/PROGRAMAÇÃO WEB/Aula_12/docker-code/www/codeigniter4/app/Views/templates/footer.php
@@ -0,0 +1,9 @@
+</article>
+    </section>
+    <footer class="p-3 rounded text-center bg-dark text-white">
+        <p>Meu footer</p>
+        <a class="link-light" href="<?=base_url('admin')?>">Área Administrativa</a>
+    </footer>
+
+</body>
+</html>
